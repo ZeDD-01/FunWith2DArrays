e7138083225fa44d2d51bd3b63018f5b38f9d6fb
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-03-01 12:20:15 +0100

    Changed the order of UserInput (row, col) Added possibility for User deciding which number should be displayed in the array grid

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -6,34 +6,39 @@ class Program
     {
         Console.WriteLine("Hi, using this program you can create 2D-arrays\nPlease enter the amount of columns and rows the array should have");
         Console.WriteLine("Enter the number of rows: ");
-        int col = int.Parse(Console.ReadLine()); 
+        int row = int.Parse(Console.ReadLine()); 
         Console.WriteLine("Enter the number of columns: ");
-        int row = int.Parse(Console.ReadLine());
+        int col = int.Parse(Console.ReadLine());
         
-        int[,] userDefinedArray = new int[col, row];
+        int[,] userDefinedArray = new int[row, col];
         
-        Console.WriteLine("You can decide what should be contained in the array\n 1. the indices of the single array elements\n 2. The number 10");
+        Console.WriteLine("You can decide what should be contained in the array\n 1. the indices of the single array elements\n 2. Choose your own number");
         Console.WriteLine("Your choice = ");
         int userDefinedContent = int.Parse(Console.ReadLine());
 
         int counter = 1;
-        for (int i = 0; i < col; i++)
+        int arrayFiller = 0;
+        if (userDefinedContent == 2)
         {
-            for (int j = 0; j < row; j++)
+            Console.WriteLine("Please choose the character the array should be filled with: ");
+            int userDefinedArrayContent = int.Parse(Console.ReadLine());
+            arrayFiller = userDefinedArrayContent;
+        }
+        else
+        {
+            arrayFiller = counter;
+        }
+        for (int rowIndex = 0; rowIndex < row; rowIndex++)
+        {
+            Console.Write("+---+\n");
+            for (int colIndex = 0; colIndex < col; colIndex++)
             {
-                if (userDefinedContent == 1)
-                {
-                    userDefinedArray[i, j] = counter;
-                    
-                }
-                if (userDefinedContent == 2)
-                {
-                    userDefinedArray[i, j] = 10;
-                }
-                Console.Write(userDefinedArray[i, j] + " | ");
+                userDefinedArray[rowIndex, colIndex] = arrayFiller;
+                
+                Console.Write("  " + userDefinedArray[rowIndex, colIndex] + "  | ");
                 counter++;
             }
-                Console.WriteLine("\n");
+            Console.Write("\n");
         }
     }
 }
