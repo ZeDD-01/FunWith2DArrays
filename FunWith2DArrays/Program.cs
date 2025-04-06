5059b50adb66744b9d03fb6defae2c530177ea7d
Manuel Wittmann <manuel.wittmann@stud.th-luebeck.de>
2025-04-06 09:54:20 +0200

    Fix - User can now enter his own character or display the index of the current array element

--- FunWith2DArrays/Program.cs
+++ FunWith2DArrays/Program.cs
@@ -1,48 +1,70 @@
-﻿namespace MyProject;
+﻿using System;
 
-class Program
+namespace MyProject
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hi, using this program you can create 2D-arrays\nPlease enter the amount of columns and rows the array should have");
-        Console.WriteLine("Enter the number of rows: ");
-        int row = int.Parse(Console.ReadLine()); 
-        Console.WriteLine("Enter the number of columns: ");
-        int col = int.Parse(Console.ReadLine());
-        
-        char[,] userDefinedArray = new char[row, col];
-        
-        Console.WriteLine("You can decide what should be contained in the array\n 1. the indices of the single array elements\n 2. Choose your own character");
-        Console.WriteLine("Your choice = ");
-        int userDefinedContent = int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, using this program you can create 2D-arrays");
+            Console.WriteLine("Please enter the amount of columns and rows the array should have");
 
-        int counter = 1;
-        char arrayFiller = ' ';
-        if (userDefinedContent == 2)
-        {
-            Console.WriteLine("Please choose the character the array should be filled with: ");
-            char userDefinedArrayContent = char.Parse(Console.ReadLine());
-            arrayFiller = userDefinedArrayContent;
-        }
-        else
-        {
-            arrayFiller = (char)counter;
-        }
-        for (int rowIndex = 0; rowIndex < row; rowIndex++)
-        {
+            Console.Write("Enter the number of rows: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of columns: ");
+            int col = int.Parse(Console.ReadLine());
+
+            string[,] userDefinedArray = new string[row, col];
+
+            Console.WriteLine("\nYou can decide what should be contained in the array:");
+            Console.WriteLine("1. The indices of the array elements");
+            Console.WriteLine("2. Choose your own character");
+            Console.Write("Your choice = ");
+            int userDefinedContent = int.Parse(Console.ReadLine());
+
+            char userDefinedChar = ' ';
+
+            if (userDefinedContent == 2)
+            {
+                Console.Write("Please choose the character the array should be filled with: ");
+                userDefinedChar = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nYour generated array:\n");
+
+            for (int rowIndex = 0; rowIndex < row; rowIndex++)
+            {
+                // Print horizontal border
+                for (int colIndex = 0; colIndex < col; colIndex++)
+                {
+                    Console.Write("+----");
+                }
+                Console.WriteLine("+");
+
+                // Fill and print cell contents
+                for (int colIndex = 0; colIndex < col; colIndex++)
+                {
+                    if (userDefinedContent == 1)
+                    {
+                        userDefinedArray[rowIndex, colIndex] = $"{rowIndex}{colIndex}";
+                    }
+                    else
+                    {
+                        userDefinedArray[rowIndex, colIndex] = $"  {userDefinedChar}";
+                    }
+
+                    Console.Write($"| {userDefinedArray[rowIndex, colIndex],2} ");
+                }
+                Console.WriteLine("|");
+            }
+
+            // Print bottom border
             for (int colIndex = 0; colIndex < col; colIndex++)
             {
-                Console.Write("+---+");
+                Console.Write("+----");
             }
-            Console.Write("\n");
-            for (int colIndex = 0; colIndex < col; colIndex++)
-            {
-                userDefinedArray[rowIndex, colIndex] = arrayFiller;
-                
-                Console.Write("| " + userDefinedArray[rowIndex, colIndex] + " |");
-                counter++;
-            }
-            Console.Write("\n");
+            Console.WriteLine("+");
         }
     }
 }
